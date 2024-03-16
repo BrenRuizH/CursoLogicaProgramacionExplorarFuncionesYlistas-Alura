ddbfda8218ed6f911440d7f01fb50a590077ea62
Bren Ruiz <ruizhernandezbrendac@gmail.com>
2024-03-15 23:07:34 -0600

    2024-03-15 Mensaje de número intentos

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let numeroSecreto = generarNumeroSecreto();
+console.log(numeroSecreto);
+let intentos = 1;
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
@@ -9,20 +11,20 @@ function asignarTextoElemento(elemento, texto) {
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
     if(numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p', 'Acertaste el número');
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos == 1) ? 'vez' : 'veces'}`);
     } else {
         if(numeroDeUsuario > numeroSecreto) {
             asignarTextoElemento('p', 'El número secreto es menor');
         } else {
             asignarTextoElemento('p', 'El número secreto es mayor');
         }
+        intentos++;
     }
     return;
 }
 
 function generarNumeroSecreto() {
     return Math.floor(Math.random()*10)+1;
-    
 }
 asignarTextoElemento('h1', 'Juego del número secreto');
 asignarTextoElemento('p', 'Indica un número del 1 al 10');
